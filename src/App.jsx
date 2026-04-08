d03ae58201f180dcec1bf66bdde8e1237cb3a75c
Juram <gimjulam2@gmail.com>
2026-04-08 17:18:15 +0900

    작업 내용 저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Settings from "./pages/Settings.jsx"
+// import Settings from "./pages/Settings.jsx"
 import Nav from './components/Nav.jsx'
+import Home from './pages/Home.jsx'
+// import Signup from "./pages/Signup.jsx"
 function App() {
 
   return (
     <>
+      {/* <Settings/> */}
+      <Home/>
       <Nav/>
-      <Settings/>
     </>
   )
 }
